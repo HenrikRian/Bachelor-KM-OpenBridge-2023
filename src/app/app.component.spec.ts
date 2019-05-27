7df65f4df46229080cb04f2e8893cf1280f274fc
Torstein I. Bø <torsteinibo@gmail.com>
2019-05-27 11:13:12 +0000

    Initial ci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('openbridge-angular');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render "button" in a a tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to openbridge-angular!');
+    expect(compiled.querySelector('a').textContent).toContain('Button');
   });
 });
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,32 +1,47 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/openbridge-angular'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    basePath: "",
+    browserDisconnectTolerance: 2,
+    browserNoActivityTimeout: 50000,
+    browsers: ["ChromeHeadless", "Chrome"],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    colors: true,
+    coverageIstanbulReporter: {
+      dir: require("path").join(__dirname, "../coverage/openbridge-angular"),
+      fixWebpackSourcePaths: true,
+      reports: ["html", "lcovonly", "text-summary"],
+    },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: "Chrome",
+        flags: [
+          "--headless",
+          "--disable-gpu",
+          "--no-sandbox",
+          "--remote-debugging-port=9222",
+        ],
+      },
+    },
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    logLevel: config.LOG_INFO,
+    plugins: [
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+    ],
+    port: 9876,
+    reporters: config.angularCli && config.angularCli.codeCoverage
+      ? ["progress", "coverage-istanbul"]
+      : ["progress", "kjhtml"],
+    restartOnFileChange: true,
     singleRun: false,
-    restartOnFileChange: true
   });
 };
