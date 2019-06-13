368a6fad8d243c1cc2801aa9e0a3ef761c391fd1
Torstein I. Bø <torstein.bo@sintef.no>
2019-06-13 13:18:41 +0200

    Changed from rem to $base-size Added top bar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {ButtonComponent} from './button/button.component';
+import {ButtonComponent} from './components/button/button.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
