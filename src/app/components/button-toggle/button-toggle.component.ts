cc144769d8a5dbb7be751004bfd96d843628fbce
Torstein I. Bø <torstein.bo@sintef.no>
2019-06-26 16:32:01 +0200

    Removed thumb-div and replaced with pseudo element ::after

---
+++ src/app/components/button-toggle/button-toggle.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-toggle',
+  templateUrl: './button-toggle.component.html',
+})
+export class ButtonToggleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
