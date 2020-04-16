c215f04402c54fbbb40804659c96eacfdddf74f9
Torstein A. Bø <torstein.bo@sintef.no>
2020-04-16 15:51:33 +0200

    Bugfix: cannot be active without focus

--- scss/components/button.stories.js
+++ scss/components/button.stories.js
@@ -1,7 +1,7 @@
 export default {title: 'Button'};
 
-export const normal = () => `<button class="ob-button ob-button--normal">Hello World</button>`;
-export const normalDisabled = () => `<button class="ob-button ob-button--normal" disabled>Hello World</button>`;
+export const normal = () => `<button class="ob-button ob-button--normal">Button</button>`;
+export const normalDisabled = () => `<button class="ob-button ob-button--normal" disabled>Button</button>`;
 export const normalWithIcon = () => `<button class="ob-button ob-button--normal">
-<span class="ob-button__icon ob-icon"><i class="material-icons">wb_sunny</i></span>Make it sunny</button>`;
-export const raised = () => '<button class="ob-button ob-button--raised">Hello World</button>';
+<span class="ob-button__icon ob-icon"><i class="material-icons">add</i></span>Button</button>`;
+export const raised = () => '<button class="ob-button ob-button--raised">Button</button>';
