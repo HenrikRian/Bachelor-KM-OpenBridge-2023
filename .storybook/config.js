83e0598c67e590097ef86042de4575aea8287225
Torstein A. Bø <torstein.bo@sintef.no>
2020-04-17 11:07:01 +0200

    Dark mode

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,54 @@
 import '!style-loader!css-loader!sass-loader!../scss/openbridge.scss';
-import '!style-loader!css-loader!sass-loader!../scss/variables-bright.scss';
+//import '!style-loader!css-loader!sass-loader!../scss/variables-bright.scss';
+import '!style-loader!css-loader!sass-loader!../scss/variables-dusk.scss';
 
+const isBright = false;
+const isDusk = true;
 
+import {addParameters} from "@storybook/html";
+import {create} from '@storybook/theming/create';
+
+let theme = {
+  brandTitle: 'OpenBridge Components',
+  brandUrl: 'https://gitlab.com/openbridge/openbridge-component',
+  brandImage: 'http://www.openbridge.no/images/OpenBridge_Logo2-p-500.png',
+};
+
+if (isBright) {
+  theme = {
+    ...theme,
+    colorPrimary: 'black',
+    colorSecondary: 'rgba(0, 0, 0, 0.9)', /*color-ui-element-active*/
+    appBg: 'rgba(234, 234, 234, 1)', /* color-ui-container-section*/
+    appContentBg: 'rgba(234, 234, 234, 1)', /* color-ui-container-section*/
+
+    barBg: '#D5D5D5', /*color-ui-container-global*/
+    barTextColor: 'rgba(0, 0, 0, 0.7)', /*color-ui-element-neutral*/
+    barSelectedColor: 'rgba(0, 0, 0, 0.9)', /*color-ui-element-active*/
+  }
+} else if (isDusk) {
+  theme = {
+    ...theme,
+    base: 'dark',
+    colorPrimary: 'black',
+    //colorSecondary: 'rgba(255, 255, 255, 1)', /*color-ui-element-active*/
+    appBg: 'rgba(54, 54, 54, 1)', /* color-ui-container-section*/
+    appContentBg: 'rgba(54, 54, 54, 1)', /* color-ui-container-section*/
+    textColor: 'white',
+    textInverseColor: 'black',
+    inputTextColor: 'black',
+
+    barBg: 'rgba(64, 64, 64, 1)',
+    barTextColor: 'rgba(255, 255, 255, 0.6)', /*color-ui-element-neutral*/
+    barSelectedColor: 'rgba(255, 255, 255, 1)', /*color-ui-element-active*/
+  }
+}
+
+const storyBookTheme = create(theme);
+
+addParameters({
+  grid: {cellSize: 8},
+  options: {
+    theme: storyBookTheme,
+  },
+});
--- .storybook/main.js
+++ .storybook/main.js
@@ -2,4 +2,5 @@ const path = require('path');
 
 module.exports = {
   stories: ['../scss/**/*.stories.[tj]s'],
+  addons: ['@storybook/addon-backgrounds/register','@storybook/addon-storysource/register']
 };
