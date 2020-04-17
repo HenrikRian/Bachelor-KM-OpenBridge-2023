89daadf6e1c912c9033b3de76a3beae2f3063363
Torstein A. Bø <torstein.bo@sintef.no>
2020-04-17 13:43:35 +0200

    Removed background toogle as it does not work with pallett background

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   stories: ['../scss/**/*.stories.[tj]s'],
-  addons: ['@storybook/addon-backgrounds/register','@storybook/addon-storysource/register', '@storybook/addon-cssresources/register']
+  addons: ['@storybook/addon-storysource/register', '@storybook/addon-cssresources/register']
 };
