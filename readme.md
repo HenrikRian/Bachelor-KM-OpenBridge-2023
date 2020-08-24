6650f83ed6fd40025ffd67e76a9d7c8e3368d06a
Torstein A. Bø <torstein.bo@sintef.no>
2020-08-24 09:07:14 +0200

    Added readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+## Openbridge-exporter ##
+This is a tool to export SVG components from Figma. 
+To run the exporter you need to acquire an token [here](https://www.figma.com/developers/api#access-tokens).
+The exporter generates SVGs and vue components.
+
+# Exports #
+A storybook demo is available [here](https://torsteinibo.gitlab.io/openbridge-exporter/).
+
+[Generated SVGs](https://gitlab.com/torsteinibo/openbridge-exporter/-/jobs/artifacts/master/browse/gen?job=build)
+
+[Generated vue.js components](https://gitlab.com/torsteinibo/openbridge-exporter/-/jobs/artifacts/master/browse/gen-vue?job=build)
--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,58 @@
 import * as fs from 'fs';
-import {convertSvg} from "./convert-svg";
-import {getFigmaFile, getFigmaSvg} from "./figmaImport";
-import {FrameNode} from "./figma-types";
-
-function replaceAll(string: string, search: string, replace: string): string {
-    return string.split(search).join(replace);
-}
-
-function makeFilename(name: string): string {
-    let out = replaceAll(name, '/', '');
-    out = replaceAll(out, ' ', '');
-    out = replaceAll(out, '#', '');
-    return out;
-}
+import { convertSvg } from './convert-svg';
+import { getFigmaFile, getFigmaSvg } from './figmaImport';
+import { FrameNode } from './figma-types';
 
 const exportComponents: { path: string[], name: string }[] = [
-    {name: 'HdgCenterLarge', path: [ 'HDG', 'HDG component', 'Large', 'Ellipse 8']},
-    {name: 'HdgArrowLarge', path: [ 'HDG', 'Elements', 'Arrow/ Large']},
-    {name: 'HdgArrowMedium', path: [ 'HDG', 'Elements', 'Arrow/ Medium']},
-    {name: 'HdgArrowSmall', path: [ 'HDG', 'Elements', 'Arrow/ Small']},
-    {name: 'HdgWatchFaceLarge', path: [ 'HDG', 'Elements', 'Watch face/ Large']},
-    {name: 'HdgWatchFaceMedium', path: [ 'HDG', 'Elements', 'Watch face/ Medium']},
-    {name: 'HdgWatchFaceSmall', path: [ 'HDG', 'Elements', 'Watch face/ Small']},
+  { name: 'HdgCenterLarge', path: ['HDG', 'HDG component', 'Large', 'Ellipse 8'] },
+  { name: 'HdgArrowLarge', path: ['HDG', 'Elements', 'Arrow/ Large'] },
+  { name: 'HdgArrowMedium', path: ['HDG', 'Elements', 'Arrow/ Medium'] },
+  { name: 'HdgArrowSmall', path: ['HDG', 'Elements', 'Arrow/ Small'] },
+  { name: 'HdgWatchFaceLarge', path: ['HDG', 'Elements', 'Watch face/ Large'] },
+  { name: 'HdgWatchFaceMedium', path: ['HDG', 'Elements', 'Watch face/ Medium'] },
+  { name: 'HdgWatchFaceSmall', path: ['HDG', 'Elements', 'Watch face/ Small'] }
 ]
 
 interface FigmaNode {
-    name: string
-    children: FigmaNode[]
-    id: string
+  name: string
+  children: FigmaNode[]
+  id: string
 }
 
 function getElement(root: FigmaNode, path: string[]): FigmaNode | null {
-    const name = path.shift();
-    if (name === undefined) {
-        return root;
-    }
-    const ele = root.children.find(value => value.name === name)
-    if (ele === undefined) return null
-    return getElement(ele, path)
+  const name = path.shift();
+  if (name === undefined) {
+    return root;
+  }
+  const ele = root.children.find(value => value.name === name)
+  if (ele === undefined) return null
+  return getElement(ele, path)
 }
 
 async function main() {
-    const mainFigmaFile = 'UNbup2BF6eR8omPSRzFSlV'
-    const documentStyles = await getFigmaFile('XXHKjGJXg0acrBak97mFhP');
-    const document = await getFigmaFile(mainFigmaFile);
-    const genFolder = 'gen'
-    if (!fs.existsSync(genFolder)) {
-        fs.mkdirSync(genFolder)
-    }
+  const mainFigmaFile = 'UNbup2BF6eR8omPSRzFSlV'
+  const documentStyles = await getFigmaFile('XXHKjGJXg0acrBak97mFhP');
+  const document = await getFigmaFile(mainFigmaFile);
+  const genFolder = 'gen'
+  if (!fs.existsSync(genFolder)) {
+    fs.mkdirSync(genFolder)
+  }
+  // @ts-ignore
+  const styles = { ...document.styles, ...documentStyles.styles }
+
+  for (const component of exportComponents) {
+
+    console.log(`Exporting ${ component.name }`)
     // @ts-ignore
-    const styles = {...document.styles, ...documentStyles.styles}
-
-    for (const component of exportComponents) {
-
-        console.log(`Exporting ${component.name}`)
-        // @ts-ignore
-        const element = getElement(document.document as FigmaNode, component.path);
-        if (element === null) {
-            console.error(`Could not find ${component.name}`)
-            continue
-        }
-        const svg = await getFigmaSvg(mainFigmaFile, element.id);
-        const out = convertSvg(element as unknown as FrameNode, svg, styles, false);
-        fs.writeFileSync(`${genFolder}/${component.name}.svg`, out)
+    const element = getElement(document.document as FigmaNode, component.path);
+    if (element === null) {
+      console.error(`Could not find ${ component.name }`)
+      continue
     }
+    const svg = await getFigmaSvg(mainFigmaFile, element.id);
+    const out = convertSvg(element as unknown as FrameNode, svg, styles, false);
+    fs.writeFileSync(`${ genFolder }/${ component.name }.svg`, out)
+  }
 }
 
 main()
