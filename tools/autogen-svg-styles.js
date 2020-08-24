910bb5064c0de115c82fe7134d44c3c06b9aa440
Torstein A. Bø <torstein.bo@sintef.no>
2020-08-24 12:11:24 +0200

    Added svg-variables

--- tools/autogen-svg-styles.js
+++ tools/autogen-svg-styles.js
@@ -3,5 +3,5 @@ const fs = require("fs");
 const pallet = fs.readFileSync('../scss/variables-bright.scss', 'utf-8').split('\r\n');
 const re = new RegExp('--(.*?-color)');
 const colorNames = pallet.map(s => s.match(re)).filter(m => m !== null).map(v => v[1]);
-const cssStrings = colorNames.map(s => `.${s}-stroke { stroke: var(--${s});}\n.${s}-fill { color: var(--${s});}\n`)
+const cssStrings = colorNames.map(s => `.ob-${s}-stroke { stroke: var(--${s});}\n.ob-${s}-fill { fill: var(--${s});}\n`)
 fs.writeFileSync('../scss/_svg-variables.scss', cssStrings.join('\n'))
