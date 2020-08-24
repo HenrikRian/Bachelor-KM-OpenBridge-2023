2a852526d86a65f47590d473855ba6238864280f
Torstein A. Bø <torstein.bo@sintef.no>
2020-08-24 12:33:36 +0200

    Switch to using css classes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
-
+import '!style-loader!css-loader!sass-loader!openbridge-components/scss/openbridge.scss';
+import '!style-loader!css-loader!sass-loader!openbridge-components/scss/variables-bright.scss';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
