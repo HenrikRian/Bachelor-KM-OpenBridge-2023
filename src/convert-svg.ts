c0fd7868af194bf63991f1688d7e88e88519cf14
Torstein A. Bø <torstein.bo@sintef.no>
2020-08-27 12:15:40 +0200

    Fixing id matching

--- src/convert-svg.ts
+++ src/convert-svg.ts
@@ -12,28 +12,47 @@ function childNodes2Elements(nodes: NodeListOf<ChildNode>): Element[] {
     return data;
 }
 
+function getSvgId(svgNode: Element): string {
+    return (svgNode.getAttribute("id") as string).replace('Â°','°');
+}
+
 export function convertSvg(figmaElement: FrameNode, svgString: string, styles: StyleDict, removeAttrs: boolean): string {
     const svgDoc = new DOMParser().parseFromString(svgString, 'text/svg')
     const root = svgDoc.firstChild;
     if (root === null) return ''
 
     for (const nd of childNodes2Elements(root.childNodes)) {
-        const svgId = nd.attributes.getNamedItem("id")?.value;
+        const svgId = getSvgId(nd)
         const elementId = figmaElement.name;
-        if (svgId !== elementId) throw "Wrong element id";
+        if (svgId !== elementId) {
+            console.warn(`Figma element id and svg id does not match (${elementId} !== ${svgId})`)
+        }
         parseNode(figmaElement.children as unknown as StyledNode[], childNodes2Elements(nd.childNodes), styles, removeAttrs);
     }
     return new XMLSerializer().serializeToString(svgDoc);
 }
 
 function parseNode(figmaElments: readonly StyledNode[], svgElements: Element[], styles: StyleDict, removeAttrs: boolean) {
-    //if (figmaElments.length !== svgElements.length) throw "Element node not equal"
     for (let i = 0; i < svgElements.length; i++) {
         const svgNode = svgElements[i];
-        const figmaNode = figmaElments[i];
+        let svgId = getSvgId(svgNode)
+        let figmaNode = figmaElments.find(f => f.name == svgId);
 
-        const stroke = svgNode.getAttribute("stroke");
-        if (stroke) {
+        if (figmaNode === undefined) {
+            const re = new RegExp('_[0-9]+$');
+            if (svgId.match(re)) {
+                svgId = svgId.replace(re, '')
+                figmaNode = figmaElments.find(f => f.name == svgId);
+            }
+        }
+
+        if (figmaNode === undefined) {
+            console.warn(`Missing figma node: ${svgId}`)
+            return
+        }
+
+        const cssClasses = []
+        if (figmaNode.styles && figmaNode.styles.stroke !== undefined) {
             if (removeAttrs) {
                 svgNode.removeAttribute("stroke")
                 svgNode.removeAttribute("stroke-opacity")
@@ -41,25 +60,30 @@ function parseNode(figmaElments: readonly StyledNode[], svgElements: Element[], 
             const strokeStyleId = figmaNode.styles.stroke;
             if (strokeStyleId !== undefined) {
                 const styleName = convertStyleName(styles[strokeStyleId].name, '-stroke');
-                svgNode.setAttribute("class", styleName)
+                cssClasses.push(styleName)
             }
         }
 
-        const fill = svgNode.getAttribute("fill");
-        if (fill) {
+        if (figmaNode.styles && figmaNode.styles.fill) {
             if (removeAttrs) {
                 svgNode.removeAttribute("fill")
             }
             const strokeStyleId = figmaNode.styles.fill;
             if (strokeStyleId !== undefined) {
                 const styleName = convertStyleName(styles[strokeStyleId].name, '-fill');
-                svgNode.setAttribute("class", styleName)
+                cssClasses.push(styleName)
             }
         }
-        svgNode.removeAttribute("id")
+        if (cssClasses.length > 0) {
+            svgNode.setAttribute("class", cssClasses.join(" "))
+        }
         const childSvgs = childNodes2Elements(svgNode.childNodes);
         if (childSvgs.length > 0) {
-            parseNode(figmaNode.children, childSvgs, styles, removeAttrs);
+            if (figmaNode.children && figmaNode.children.length > 0) {
+                parseNode(figmaNode.children, childSvgs, styles, removeAttrs);
+            } else {
+                console.warn(`Missing figma node on ${svgId}. Skipping subnodes`)
+            }
         }
 
     }
