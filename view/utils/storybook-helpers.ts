e5f63fab52fc3da91fd9a43f2e4e1241e170636e
Torstein A. Bø <torstein.bo@sintef.no>
2021-08-18 10:11:19 +0200

    Fix build

--- view/utils/storybook-helpers.ts
+++ view/utils/storybook-helpers.ts
@@ -1,15 +1,16 @@
 export function mapBooleanArgs(args: any): any {
-    const out = args;
-    Object.entries(args).forEach(
-        (entry) => {
-            const k = entry[0];
-            let v = entry[1];
-            if (v === true) {
-                v = ''
-            } else if (v === false) {
-                v = undefined
-            }
-            out[k] = v
-        })
-    return out
+  const out: any = {};
+  Object.entries(args).forEach(
+    (entry) => {
+      const k = entry[0];
+      const v = entry[1];
+      if (v === true) {
+        out[k] = ''
+      } else if (v !== false) {
+        out[k] = v
+      }
+
+
+    })
+  return out
 }
